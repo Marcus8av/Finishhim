78ba01617061c9d7ca9062a1f00e2289aa049a75
markus b <mnogo_bukof@mail.ru>
2022-12-01 15:09:16 +0700

    .

--- Задание/Program.cs
+++ Задание/Program.cs
@@ -2,7 +2,7 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
 
-string[] Array = new string[10] {"Love", "In", "Des", "Chanel", "LoL", "invite", "kek", "Good", "cool", "lim",};
+string[] Array = new string[10] {"Love", "In", "Des", "Chanel", "LoL", "invite", "kek", "Good", "cool", "lim",}; // задаем исхродный код
 string[] newarray = new string[Array.Length];
 void array(string[] array, string[] newarray )
 { 
